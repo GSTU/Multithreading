fdf32e90499b38cac203a61cbfc7124ff5467cc3
Igor Nesterenya <nesterione@gmail.com>
2015-03-31 23:07:57 +0300

    Change manager algorithm to RR

--- laba1_example/src/laba1_example/Process.java
+++ laba1_example/src/laba1_example/Process.java
@@ -8,6 +8,8 @@ public class Process {
 	private double[][] A;
 	private double[] B;
 	
+	private boolean isCompleeted = false;
+	
 	public Process(double[][] A, double[] B) {
 		this.A = A;
 		this.B = B;
@@ -15,8 +17,47 @@ public class Process {
 		number = counter;
 	}
 	
-	public void solve() throws InterruptedException {
+	/*public void solve() throws InterruptedException {
 		System.out.println("Work process: "+number);
 		Thread.sleep(1000);
+	}*/
+	
+	public boolean isCompleted() {
+		return isCompleeted;
 	}
+	
+	private int  i = 0;
+	private int j = 0;
+	
+	int len = 5;
+	public void step() throws InterruptedException {
+		
+		for(; i< A.length;) {
+			for(; j < A[0].length; j++) {
+				
+			}
+			
+			System.out.println(number + ": "+ i);
+			
+			i++;
+			break;
+		}
+		
+		
+		
+		//if(isCompleted()) return;
+		
+		/*System.out.println("Work process: "+number );
+		len--;
+		
+		if(len==0) {
+			System.out.println("Work process " + number + " was COMPLETED");
+			isCompleeted = true;
+		}*/
+		
+		Thread.sleep(500);
+	}
+	
+	public void pause() {};
+	public void resume() {};
 }
--- laba1_example/src/laba1_example/ProcessManager.java
+++ laba1_example/src/laba1_example/ProcessManager.java
@@ -13,8 +13,43 @@ public class ProcessManager {
 	}
 	
 	public void run() throws InterruptedException {
-		for(Process process : processes) {
-			process.solve();
+		int cvarks = 3;
+		int current = 0;
+		int activeProcess = 0;
+		
+		while(checkQueue()) {
+			while(current < cvarks) {
+				processes.get(activeProcess).step();
+				current++;
+			}
+			current=0;
+			
+			activeProcess = changeActiveProcess(activeProcess);
 		}
+		
+		System.out.println("All processes was completed!!!");
 	}
+	
+	private int changeActiveProcess(int activeProcess) {		
+		activeProcess++;
+		
+		while(processes.get(activeProcess).isCompleted()) {
+			activeProcess++;
+		}
+
+		if(activeProcess >= (processes.size()-1)) {
+			activeProcess = 0;
+		}
+		return activeProcess;
+	}
+	
+	private boolean checkQueue() {
+		
+		for(Process process : processes) {
+			if(!process.isCompleted()) { return true; }
+		}
+		
+		return false;
+	}
+
 }
