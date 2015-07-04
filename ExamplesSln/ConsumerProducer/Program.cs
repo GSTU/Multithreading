91954874b7112b67b3ead3fa4f3177488acf0445
Igor Nesterenya <nesterione@gmail.com>
2015-07-05 00:20:03 +0300

    Add example P/C not synchronized

---
+++ ExamplesSln/ConsumerProducer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsumerProducer {
+  class Program {
+
+    const int REPEAT_TIMES = 10;
+
+    static int buffer;
+
+    static void Producer() {
+      Random random = new Random();
+      for (int i = 0; i < REPEAT_TIMES; i++) {
+        buffer = random.Next(100);
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("producer [{0}]: I put {1} in buffer", i, buffer);
+        Console.ForegroundColor = ConsoleColor.White;
+      }
+    }
+
+    static void Consumer() {
+
+      for (int i = 0; i < REPEAT_TIMES; i++) {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("consumer [{0}]: I get {1} from buffer", i, buffer);
+        Console.ForegroundColor = ConsoleColor.White;
+      }
+    }
+
+
+    static void Main(string[] args) {
+
+      Thread producer = new Thread(x => Producer());
+      Thread consumer = new Thread(x => Consumer());
+      producer.Start();
+      consumer.Start();
+
+      producer.Join();
+      consumer.Join();
+      Console.ReadKey();
+    }
+  }
+}
